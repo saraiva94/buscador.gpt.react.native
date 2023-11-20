a67621564995bb668b34b2d327544ae1c1c2cf4a
Swamiy <swami_gs@live.com>
2023-11-20 11:32:21 -0300

    Atualizações diárias

--- App.jsx
+++ App.jsx
@@ -5,10 +5,10 @@ import C1 from './Componentes/comp';
 export default function App1() {
   return (
     <View style={estilos.container}>
-      <C1 />
-      <C1 />
-      <C1 />
-      <C1 />
+      <C1 curso="React Native" nota="undefined" cor="#ff0" font="30" />
+      <C1 curso="C++" nota="undefined" cor="#0f0" font="30" />
+      <C1 curso="Javascript" nota="undefined" cor="#0ff" font="25" />
+      <C1 curso="c#" nota="undefined" cor="#f00" font="32" />
       <Text style={estilos.txt0}>youtube.com/cfbcursos</Text>
       <Text style={styles.text}>cfbcursos.com.br</Text>
     </View>
@@ -25,11 +25,10 @@ const styles = {
 const estilos = StyleSheet.create({
   txt0: {
     color: '#f0f',
-    fontSize: 30,
+    fontSize: 35,
   },
   container: {
     flex: 1,
-    BackgroundColor: '#ffa600',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- Componentes/comp.jsx
+++ Componentes/comp.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import {StyleSheet, Text} from 'react-native';
 
-export default () => {
-  return <Text style={estilos.txt3}>CFB Cursos - curso de react native</Text>;
-};
+let nt;
+let cor;
+let font;
+
+export default function (props) {
+  font = parseInt(props.font);
+  nt = props.nota;
+  cor = props.cor;
+  return (
+    <Text
+      style={StyleSheet.compose(estilos.txt3, {color: cor, fontSize: font})}>
+      CFB Cursos - curso de {props.curso} : {nt}
+    </Text>
+  );
+}
 
 const estilos = StyleSheet.create({
   txt1: {
@@ -11,11 +23,11 @@ const estilos = StyleSheet.create({
     fontSize: 30,
   },
   txt2: {
-    color: '#00f',
+    // color: '#00f',
     fontSize: 30,
   },
   txt3: {
-    color: '#0f0',
-    fontSize: 30,
+    // color: '#0f0',
+    fontSize: 20,
   },
 });
