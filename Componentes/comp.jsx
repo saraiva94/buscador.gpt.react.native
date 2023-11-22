c726c15f76f6ce5f5a3db128bbf4d748980e4574
Swamiy <swami_gs@live.com>
2023-11-22 13:03:23 -0300

    Atualização diária

--- Componentes/comp.jsx
+++ Componentes/comp.jsx
@@ -1,33 +1,17 @@
 import React from 'react';
 import {StyleSheet, Text} from 'react-native';
+import style from '../Css/style';
 
 let nt;
-let cor;
 let font;
 
 export default function (props) {
   font = parseInt(props.font, 10);
   nt = props.nota;
-  cor = props.cor;
+
   return (
-    <Text
-      style={StyleSheet.compose(estilos.txt3, {color: cor, fontSize: font})}>
+    <Text style={StyleSheet.compose(style.textStyle, {fontSize: font})}>
       CFB Cursos - curso de {props.curso} : {nt}
     </Text>
   );
 }
-
-const estilos = StyleSheet.create({
-  txt1: {
-    color: '#f00',
-    fontSize: 30,
-  },
-  txt2: {
-    // color: '#00f',
-    fontSize: 30,
-  },
-  txt3: {
-    // color: '#0f0',
-    fontSize: 20,
-  },
-});
---
+++ Css/style.jsx
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    color: '#9400d3',
+  },
+  textTitle: {
+    color: '#daa520',
+  },
+  textPattern: {
+    color: '#ff0000',
+    fontSize: 30,
+  },
+});
