f08b92d31de041f72c3788ab939163afa92f82b3
Swamiy <swami_gs@live.com>
2023-11-26 19:23:27 -0300

    Projeto buscador

--- App.jsx
+++ App.jsx
@@ -1,36 +1,75 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import style from './Css/style';
-import box from './Componentes/boxes';
-import Boxes from './Componentes/boxes';
+import {StyleSheet, View, Text, FlatList} from 'react-native';
+import React, {useState} from 'react';
+import Axios from 'axios';
 
 export default function App1() {
+  const [data, setData] = useState([]);
+  const apiKey = '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';
+  const apiUrl =
+    'https://api.openai.com/v1/engines/text-davinci-002/completions';
+  const [textInput, setTextInput] = useState('');
+
+  const handleSend = async () => {
+    const prompt = textInput;
+    const response = await Axios.post(
+      apiUrl,
+      {
+        prompt: prompt,
+        max_tokens: 1024,
+        temperature: 0.5,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${apiKey}`,
+        },
+      },
+    );
+    const text = response.data.choices[0].text;
+    setData([
+      ...data,
+      {type: 'user', text: textInput},
+      {type: 'bot', text: text},
+    ]);
+    setTextInput: '';
+  };
+
   return (
-    <View style={estilos.container}>
-      <Boxes />
-      <Text style={estilos.txt0}>youtube.com/cfbcursos</Text>
-      <View style={StyleSheet.compose(style.space)} />
-      <Text style={styles.text}>cfbcursos.com.br</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>AI ChatBot</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item, index) => index.toString}
+        style={styles.body}
+        renderItem={({item}) => (
+          <View style={{flexDirection: 'row', padding: 10}}>
+            <Text
+              style={{
+                fontWeight: 'bold',
+                color: item.type === 'user' ? 'blue' : 'green',
+              }}>
+              {item.type === 'use' ? 'User' : 'Bot'}
+            </Text>
+            <Text />
+          </View>
+        )}
+      />
     </View>
   );
 }
 
-const styles = {
-  text: {
-    color: '#2b59ff',
-    fontSize: 50,
-  },
-};
-
-const estilos = StyleSheet.create({
-  txt0: {
-    color: '#f0f',
-    fontSize: 35,
-  },
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#e6c9ff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    color: '#870f0f',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    marginTop: 70,
   },
 });
 
