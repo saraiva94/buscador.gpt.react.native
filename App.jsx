842f9d4bab14196578bd945f5c159908212096c0
Swamiy <swami_gs@live.com>
2023-12-01 10:46:05 -0300

    Atualizações de rotina

--- App.jsx
+++ App.jsx
@@ -1,95 +1,63 @@
 import React, {useState} from 'react';
 import {
-  FlatList,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
-import axios from 'axios';
 
-export default function App1() {
-  const [data, setData] = useState([]);
-  const apiKey = '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';
-  const apiUrl = 'https://api.openai.com/v1/chat/completions';
-  const [textInput, setTextInput] = useState('');
+export default function App2() {
+  const [altura, setAltura] = useState('');
+  const [genero, setGenero] = useState('');
+  const [pesoIdealResult, setPesoIdealResult] = useState('');
 
-  const handleSend = async () => {
-    try {
-      const response = await axios.post(
-        apiUrl,
-        {
-          prompt: textInput,
-          max_tokens: 1024,
-          temperature: 0.5,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${apiKey}`,
-          },
-        },
+  const calcularPesoIdeal = () => {
+    let pesoIdeal;
+
+    if (genero.toLowerCase() === 'masculino') {
+      pesoIdeal = 72.7 * parseFloat(altura) - 58;
+    } else if (genero.toLowerCase() === 'feminino') {
+      pesoIdeal = 62.1 * parseFloat(altura) - 44.7;
+    } else {
+      console.error(
+        'Gênero não reconhecido. Por favor, insira "masculino" ou "feminino".',
       );
-      if (
-        response &&
-        response.data &&
-        response.data.choices &&
-        response.data.choices[0]
-      ) {
-        const text = response.data.choices[0].text;
-        setData([
-          ...data,
-          {type: 'user', text: textInput},
-          {type: 'bot', text: text},
-        ]);
-      }
-    } catch (error) {
-      console.log(error); // Registrar o objeto de erro completo no console
-
-      if (error.response) {
-        console.error(
-          'Erro de resposta:',
-          error.response.status,
-          error.response.data,
-        );
-      } else if (error.request) {
-        console.error('Erro de requisição:', error.request);
-      } else {
-        console.error('Erro:', error.message);
-      }
+      return;
     }
-    setTextInput('');
+
+    setPesoIdealResult(pesoIdeal.toFixed(2));
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>AI ChatBot</Text>
-      <FlatList
-        data={data}
-        keyExtractor={(item, index) => index.toString()}
-        style={styles.body}
-        renderItem={({item}) => (
-          <View
-            style={
-              item.type === 'user' ? styles.userMessage : styles.botMessage
-            }>
-            <Text style={styles.messageText}>
-              {item.type === 'user' ? 'User: ' : 'Bot: '}
-              {item.text}
-            </Text>
-          </View>
-        )}
-      />
+      <Text style={styles.title}>Calculadora de Peso Ideal</Text>
+      <Text style={styles.title2}>Digite a altura abaixo</Text>
+
       <TextInput
         style={styles.input}
-        value={textInput}
-        onChangeText={text => setTextInput(text)}
-        placeholder="Ask me anything"
+        value={altura.toString()}
+        onChangeText={text => setAltura(text)}
+        placeholder="Altura (em metros)"
+        keyboardType="numeric"
       />
-      <TouchableOpacity style={styles.button} onPress={handleSend}>
-        <Text style={styles.buttonText}>Submit</Text>
+      <Text style={styles.title2}>Escolha o sexo</Text>
+      <TextInput
+        style={styles.input}
+        value={genero}
+        onChangeText={text => setGenero(text)}
+        placeholder="Gênero (masculino ou feminino)"
+      />
+      <TouchableOpacity style={styles.button} onPress={calcularPesoIdeal}>
+        <Text style={styles.buttonText}>Calcular</Text>
       </TouchableOpacity>
+
+      {pesoIdealResult ? (
+        <View style={styles.resultContainer}>
+          <Text style={styles.resultText}>Peso Ideal:</Text>
+          <Text style={styles.result}>{pesoIdealResult} kg</Text>
+        </View>
+      ) : null}
     </View>
   );
 }
@@ -99,32 +67,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#c395ec92',
     alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     color: '#6a03ca',
     fontSize: 28,
     fontWeight: 'bold',
     marginBottom: 20,
-    marginTop: 70,
   },
-  body: {
-    width: '100%',
-    marginBottom: 10,
-  },
-  userMessage: {
-    flexDirection: 'row',
-    padding: 10,
-    justifyContent: 'flex-end',
-  },
-  botMessage: {
-    flexDirection: 'row',
-    padding: 10,
-    justifyContent: 'flex-start',
-  },
-  messageText: {
+  title2: {
+    color: '#000000',
+    fontSize: 21,
     fontWeight: 'bold',
-    fontSize: 16,
-    color: 'black',
+    marginBottom: 20,
   },
   input: {
     borderWidth: 1,
@@ -134,6 +89,8 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderRadius: 10,
     paddingHorizontal: 10,
+    backgroundColor: 'white',
+    color: 'black',
   },
   button: {
     backgroundColor: 'white',
@@ -148,5 +105,23 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: 'bold',
     color: 'blue',
+  },
+  resultContainer: {
+    backgroundColor: 'white',
+    width: '90%',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    padding: 10,
+  },
+  resultText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  result: {
+    fontSize: 18,
+    color: 'black',
   },
 });
